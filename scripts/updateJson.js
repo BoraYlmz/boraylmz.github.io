172d7dc91ed505eb282d135fea55544f33aaee2a
Bora Yılmaz <72613208+BoraYlmz@users.noreply.github.com>
2025-11-28 11:21:56 +0300

    Update updateJson.js

--- scripts/updateJson.js
+++ scripts/updateJson.js
@@ -3,22 +3,32 @@ const path = require('path');
 const matter = require('gray-matter');
 
 const jsonFilePath = path.join(__dirname, '../postdb.json');
-let postData = JSON.parse(fs.readFileSync(jsonFilePath, 'utf-8'));
+
+let postData = { posts: [] };
+
+try {
+  const rawData = fs.readFileSync(jsonFilePath, 'utf-8');
+  if (rawData) {
+    postData = JSON.parse(rawData);
+  }
+} catch (error) {
+  console.error('JSON dosyası okunurken bir hata oluştu:', error);
+}
 
 const postsDirectory = path.join(__dirname, '../posts');
 
 fs.readdirSync(postsDirectory).forEach(file => {
-  if (file.endsWith('.md')) {
+  if (file.endsWith('.md') && fs.statSync(path.join(postsDirectory, file)).isFile()) {
     const filePath = path.join(postsDirectory, file);
     const content = fs.readFileSync(filePath, 'utf-8');
     const { data } = matter(content);
 
     postData.posts.push({
-      title: data.title,
-      category: data.category,
-      date: data.date,
-      summary: data.summary,
-      image: data.image,
+      title: data.title || 'Başlık bulunamadı',
+      category: data.category || 'Kategori bulunamadı',
+      date: data.date || 'Tarih bulunamadı',
+      summary: data.summary || 'Özet bulunamadı',
+      image: data.image || 'Resim bulunamadı',
       file: file
     });
   }
